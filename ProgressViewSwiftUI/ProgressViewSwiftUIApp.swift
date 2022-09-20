1d629ae3e1cf669b38266ec41699c45c44d4b4dd
clownami <roccoyt228@gmail.com>
2022-09-20 10:31:38 +0300

    Initial Commit

---
+++ ProgressViewSwiftUI/ProgressViewSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProgressViewSwiftUIApp.swift
+//  ProgressViewSwiftUI
+//
+//  Created by Клоун on 20.09.2022.
+//
+
+import SwiftUI
+
+@main
+struct ProgressViewSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
